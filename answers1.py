efc44c113c48d26e1018500cef1cb5e89629e928
Myiah Moore <myiahsade@gmail.com>
2018-07-13 07:08:27 -0500

    check in

--- answers1.py
+++ answers1.py
@@ -1,24 +1,25 @@
 
 #ex1
-#for x in range(-20 , 51):
- #   print(x)
+for x in range(-20 , 51):
+    print(x)
 
 
 #ex2
-#for x in range(0 , 21 , 2):
- #   print(x)
+for x in range(0 , 21 , 2):
+    print(x)
 
 
 #ex3
-#group = [1, 2, 3, 4]
-#answer = sum(group)
-#print (answer)
+group = [1, 2, 3, 4]
+answer = sum(group)
+print (answer)
 
 
 #ex4
-#for x in range(0 , 101 , 4):
- #   print (x)
+for x in range(0 , 101 , 4):
+   print (x)
 
+ #exEXTRA
 question = (input("what is your code?: Enter Q to quit: "))
 doYouQuit = 'no'
 while(doYouQuit == 'no'):
